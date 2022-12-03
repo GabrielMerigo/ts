09586a914c9e038b4c95c6d75eb05a627e64e5ae
Gabriel Merigo <gabriel.merigo.dev@gmail.com>
2022-12-03 17:43:06 -0300

    Delete server.ts

--- server.ts
+++
@@ -1,7 +0,0 @@
-import express from 'express';
-
-const app = express();
-
-app.get('/', (request, response) => response.json({ message: 'Hello Naldi!' }));
-
-app.listen(3350, () => console.log('Server is running!!!!'));
